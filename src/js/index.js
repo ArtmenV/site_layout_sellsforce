12d9fd110030204d3d9bf638795132ff8ca94f94
Artmen_V <viliutich.artem@gmail.com>
2019-06-20 23:47:10 +0700

    lauout-head-menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+// import '../assets/scss/navigation/navigation.scss'   
+
+
+
+// const sandwich = document.body.querySelector('.sandwich');
+//    sandwich.onclick = function() {
+//     sandwich.classList.toggle('sandwich', 'sandwich--active');
+//    }();
+
+   
+
+    const sandwich = document.body.querySelector('.sandwich');
+    // sandwich.addEventListener('click')
+    sandwich.onclick = function() {
+     sandwich.classList.toggle('sandwich', 'sandwich--active');
+    }(); 
